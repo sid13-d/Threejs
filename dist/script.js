440c9baf15643485b25c8c5410b0e1e8bd93795c
Siddhesh Shinde <shindesiddhesh13062003@gmail.com>
2022-02-12 23:53:59 +0530

    adding three directory

--- dist/script.js
+++ dist/script.js
@@ -1,8 +1,8 @@
 import './style.css'
 import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { OrbitControls } from './three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'dat.gui'
-import typefaceFont from 'three/examples/fonts/helvetiker_regular.typeface.json'
+import typefaceFont from './three/examples/fonts/helvetiker_regular.typeface.json'
 
 /**
  * Base
